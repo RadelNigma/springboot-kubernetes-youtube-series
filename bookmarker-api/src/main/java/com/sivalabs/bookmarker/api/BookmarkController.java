5412eab154f396a8a78d3471e06b484a79e761dd
rad <radelnigma@yandex.ru>
2023-09-21 21:50:46 +0400

    21.09_2

--- bookmarker-api/src/main/java/com/sivalabs/bookmarker/api/BookmarkController.java
+++ bookmarker-api/src/main/java/com/sivalabs/bookmarker/api/BookmarkController.java
@@ -1,14 +1,12 @@
 package com.sivalabs.bookmarker.api;
 
-import com.sivalabs.bookmarker.domain.Bookmark;
 import com.sivalabs.bookmarker.domain.BookmarkService;
+import com.sivalabs.bookmarker.domain.BookmarksDTO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/api/bookmarks")
@@ -17,7 +15,7 @@ public class BookmarkController {
     private final BookmarkService bookmarkService;
 
     @GetMapping
-    public List<Bookmark> getBookmarks(@RequestParam(name = "page", defaultValue = "1") Integer page) {
+    public BookmarksDTO getBookmarks(@RequestParam(name = "page", defaultValue = "1") Integer page) {
         return bookmarkService.getBookmark(page);
     }
 }
--- bookmarker-api/src/main/java/com/sivalabs/bookmarker/domain/BookmarkService.java
+++ bookmarker-api/src/main/java/com/sivalabs/bookmarker/domain/BookmarkService.java
@@ -1,13 +1,12 @@
 package com.sivalabs.bookmarker.domain;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
 
 @Service
 @Transactional
@@ -16,9 +15,10 @@ public class BookmarkService {
     private final BookmarkRepository repository;
 
     @Transactional(readOnly = true)
-    public List<Bookmark> getBookmark(Integer page) {
+    public BookmarksDTO getBookmark(Integer page) {
         int pageNo = page < 1 ? 0 : page - 1;
-        Pageable pageable = PageRequest.of(pageNo, 3, Sort.Direction.DESC, "createdAt");
-        return repository.findAll(pageable).getContent();
+        Pageable pageable = PageRequest.of(pageNo, 10, Sort.Direction.DESC, "createdAt");
+        Page<BookmarkDTO> bookmarkPage = repository.findBookmark(pageable);
+        return new BookmarksDTO(bookmarkPage);
     }
 }
